2cf7afb63ccd39457bcd1dbd5c486dd89a13241e
r2dong <r2dong@yahoo.com>
2018-10-04 02:11:08 -0500

    firebase working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
+
+// the following two lines required for firebase real time database
+import { environment } from '../../environments/environment'
+firebase.initializeApp(environment.firebase)
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent implements OnInit {
+  
+  user_name : String
+
+  constructor() {
+    
+    // a simple query to fire base real time data base
+    firebase.database().ref('/user/1/user_name').once('value').then(snapshot => {
+      this.user_name = snapshot.val()
+      console.log(this.user_name)
+    })
+  }
 
   ngOnInit() {
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC2z-vNdnhkE6S6T3SDiPnzc_yW_oqM0qI",
+    authDomain: "chat-bot-574d2.firebaseapp.com",
+    databaseURL: "https://chat-bot-574d2.firebaseio.com",
+    projectId: "chat-bot-574d2",
+    storageBucket: "chat-bot-574d2.appspot.com",
+    messagingSenderId: "115249993823"
+  }
 };
