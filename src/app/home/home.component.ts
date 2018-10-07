869841fce35f28030d4adcfcdb07bb31c6ccff09
r2dong <r2dong@yahoo.com>
2018-10-06 22:00:09 -0500

    simple and ugly login working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase'
+import * as firebase from 'firebase';
 
 // the following two lines required for firebase real time database
-import { environment } from '../../environments/environment'
-firebase.initializeApp(environment.firebase)
+// import { environment } from '../../environments/environment';
+// firebase.initializeApp(environment.firebase);
 
 @Component({
   selector: 'app-home',
@@ -12,16 +12,16 @@ firebase.initializeApp(environment.firebase)
 })
 
 export class HomeComponent implements OnInit {
-  
-  user_name : String
+
+  user_name: String;
 
   constructor() {
-    
+
     // a simple query to fire base real time data base
     firebase.database().ref('/users/1/user_name').once('value').then(snapshot => {
-      this.user_name = snapshot.val()
-      console.log(this.user_name)
-    })
+      this.user_name = snapshot.val();
+      console.log(this.user_name);
+    });
   }
 
   ngOnInit() {
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebaseui from 'firebaseui';
+import * as firebase from 'firebase';
+import { environment } from '../../environments/environment';
+firebase.initializeApp(environment.firebase);
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
+    // const uiConfig = {
+    //   callbacks: {
+    //     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+    //       // User successfully signed in.
+    //       // Return type determines whether we continue the redirect automatically
+    //       // or whether we leave that to developer to handle.
+    //       return true;
+    //     },
+    //     uiShown: function() {
+    //       // The widget is rendered.
+    //       // Hide the loader.
+    //       document.getElementById('loader').style.display = 'none';
+    //     }
+    //   },
+    //   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    //   signInFlow: 'popup',
+    //   signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    //   signInOptions: [
+    //     // Leave the lines as is for the providers you want to offer your users.
+    //     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //     firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //     firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    //     firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //     firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    //   ],
+    //   // Terms of service url.
+    //   tosUrl: '<your-tos-url>',
+    //   // Privacy policy url.
+    //   privacyPolicyUrl: '<your-privacy-policy-url>'
+    // };
+
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // ui.start('#firebaseui-auth-container', uiConfig);
+    // const uiConfig = {
+    //   callbacks: {
+    //     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+    //       // User successfully signed in.
+    //       // Return type determines whether we continue the redirect automatically
+    //       // or whether we leave that to developer to handle.
+    //       return true;
+    //     },
+    //     uiShown: function() {
+    //       // The widget is rendered.
+    //       // Hide the loader.
+    //       document.getElementById('loader').style.display = 'none';
+    //     }
+    //   },
+    ui.start('#firebaseui-auth-container', {
+      callbacks: {
+        uiShown: function () {
+          // Hide the loader. The widget is rendered.
+          document.getElementById('loader').style.display = 'none';
+        }
+      },
+      signInSuccessUrl: '/home',
+      signInFlow: 'popup',
+      queryParameterForWidgetMode: 'select',
+      // signInFlow: 'redirect',
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      // Other config options...
+    });
   }
 
+  constructor() {}
 }
