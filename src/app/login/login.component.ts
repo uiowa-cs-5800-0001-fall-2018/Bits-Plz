4e3fa6c389760ce2acd6796aa390f4a0045688d6
Rentian Dong <r2dong@yahoo.com>
2018-10-13 13:07:55 -0500

    refactor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,42 +10,30 @@ import {FirebaseService} from '../services/firebase.service';
 
 export class LoginComponent implements OnInit {
 
-  ui: firebaseui.auth.AuthUI;
   firebase_service: FirebaseService;
   user_name: String;
   user_email: String;
 
-  ngOnInit() {
-    console.log('init started');
-    // if (this.user_name === null) {
-    //   this.ui.start('#firebaseui-auth-container', this.firebase_service.get_config());
-    // }
-    console.log('init finished');
-  }
+  ngOnInit() { }
 
   ngAfterViewInit() {
-    console.log('view_init')
+    this.start_ui();
+  }
+
+  start_ui(): void {
     if (this.user_name === null) {
-      this.ui.start('#firebaseui-auth-container', this.firebase_service.get_config());
+      this.firebase_service.get_login_ui().start(
+        '#firebaseui-auth-container',
+        this.firebase_service.get_config()
+      );
     }
   }
 
-  // start_ui(): void {
-  //  this.ui.start('#firebaseui-auth-container', this.firebase_service.get_config());
-  // }
-
   constructor(firebase_service: FirebaseService) {
-    console.log('constructor started');
     this.firebase_service = firebase_service;
-    this.ui = firebase_service.get_login_ui();
     this.user_name = sessionStorage.getItem('user_name');
     this.user_email = sessionStorage.getItem('user_email');
-    console.log('constructor finished');
   }
-
-  // login(): void {
-  //  this.ui.start('#firebaseui-auth-container', this.firebase_service.get_config());
-  // }
 
   logout(): void {
     sessionStorage.clear();
