c09f4428da9f0baeec71a48e488be64f2055706e
r2dong <r2dong@yahoo.com>
2018-10-31 10:16:42 -0500

    set up simple query to server

--- src/app/services/twitter.service.ts
+++ src/app/services/twitter.service.ts
@@ -5,6 +5,16 @@ import * as request from 'request-promise-native';
 @Injectable()
 export class TwitterService {
 
-  constructor() { }
+  constructor() {
+    const post_str = 'node server deployed at https://bits-plz-backend.herokuapp.com/search';
 
+    console.log('request starting');
+    request.post(post_str, (error, response, body) => {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML of the response
+    }).then(() => {
+      console.log('request finished');
+    });
+  }
 }
