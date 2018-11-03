cb7ea6df9764058e672a43bcef761a22bd7850da
Rentian Dong <r2dong@yahoo.com>
2018-11-03 13:38:40 -0500

    remove obsolete code

--- src/app/services/twitter.service.ts
+++ src/app/services/twitter.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as request from 'request-promise-native';
+import { InputEntryModel } from './inputEntry.model';
 
 
 @Injectable()
@@ -9,13 +10,18 @@ export class TwitterService {
     const post_str = 'https://bits-plz-backend.herokuapp.com/search';
 
     console.log('request starting');
-    // request.post(post_str, {mode: 'no-cors'}, (error, response, body) => {
-    request.post(post_str, (error, response, body) => {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    request.post(post_str, (error, response, body) => { // 'response' is the actual content
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
       console.log('body:', body); // Print the HTML of the response
+      console.log('body type', typeof body);
     }).then(() => {
       console.log('request finished');
     });
   }
+
+  parse_response(raw: string): InputEntryModel[] {
+    return null;
+  }
+
 }
