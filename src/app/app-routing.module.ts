27878877f7585e42c238f7f52c6db4b346cf466c
Rentian Dong <r2dong@yahoo.com>
2018-11-12 16:47:56 -0600

    remove explicit route to result display component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { BlocklyComponent } from './blockly/blockly.component';
-import {ResultDisplayComponent} from './result-display/result-display.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'blockly', component: BlocklyComponent},
-  { path: 'result-display', component: ResultDisplayComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
