b806e2181376251c997f74493c44b1dd7369a10d
Rentian Dong <r2dong@yahoo.com>
2018-12-02 16:53:26 -0600

    remove obsolete code

--- src/app/services/twitter.service.ts
+++ src/app/services/twitter.service.ts
@@ -7,24 +7,10 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class TwitterService {
 
-  private static parse_response(raw): ResultModel[] {
-    const tweet_list: ResultModel[] = [];
-    for (const tweet of raw.statuses) {
-      tweet_list.push({
-        title: null,
-        region: tweet.user.location === '' ? null : tweet.user.location,
-        time: tweet.created_at,
-        content: tweet.text,
-        score: 0
-      });
-    }
-    return tweet_list;
-  }
-
   constructor() { }
 
   public get_tweets(): Observable<ResultModel[]> {
-    const post_str = 'https://bits-plz-backend.herokuapp.com/search';//?keyword=hi
+    const post_str = 'https://bits-plz-backend.herokuapp.com/search';
     return new Observable(ob => {
       request.post(post_str, (error, response, body) => { // 'body': string is the actual content
         ob.next(parseJson(body));
