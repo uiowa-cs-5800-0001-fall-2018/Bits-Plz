09eca39edff224c3228a973274783a79d7404051
abubiy <anjela-bubiy@uiowa.edu>
2018-12-03 10:48:36 -0600

    added Contact Us button on home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+
 import { HomeComponent } from './home/home.component';
 import { BlocklyComponent } from './blockly/blockly.component';
 
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'blockly', component: BlocklyComponent},
-  { path: '**', component: NotFoundComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
-import { NotFoundComponent } from './not-found/not-found.component';
+
 import { HomeComponent } from './home/home.component';
 import { BlocklyComponent } from './blockly/blockly.component';
 import { FirebaseService } from './services/firebase.service';
@@ -20,7 +20,7 @@ import {BlocksService} from './blocks.service';
   declarations: [
     AppComponent,
     LoginComponent,
-    NotFoundComponent,
+
     HomeComponent,
     BlocklyComponent,
     ResultDisplayComponent
