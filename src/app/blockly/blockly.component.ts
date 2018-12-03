4ba666a04fa1d886bb08eaf9f4c838d4b90550d6
Rentian Dong <r2dong@yahoo.com>
2018-12-02 18:51:06 -0600

    non-functioning three button swal working

--- src/app/blockly/blockly.component.ts
+++ src/app/blockly/blockly.component.ts
@@ -6,6 +6,7 @@ import {ResultModel} from '../services/result.model';
 import {ResultDisplayComponent} from '../result-display/result-display.component';
 import {BlocksService} from '../blocks.service';
 import swal from 'sweetalert2';
+import * as $ from 'jquery';
 
 
 @Component({
@@ -16,7 +17,7 @@ import swal from 'sweetalert2';
 export class BlocklyComponent implements OnInit {
 
   resultDisplay;
-  workspace_list: {id, name}[];
+  workspace_list: { id, name }[];
 
   constructor(
     private flashMessagesService: FlashMessagesService,
@@ -51,17 +52,75 @@ export class BlocklyComponent implements OnInit {
     const usersRef = this.firebaseService.database().ref(user_name);
     BlocksService.inject_blocks('blocklyDiv');
     usersRef.on('value', (data) => {
-        this.workspace_list = [];  // TODO add just the workspace changed (O(1) time, currently O(n))
-        const savespace = data.val();
-        const keys = Object.keys(savespace);
-        for (let i = 0; i < keys.length; i++) {
-          const k = keys[i];
-          const name = savespace[k].name;
-          this.workspace_list.push( { 'id': i, 'name': name} );
-        }
+      this.workspace_list = [];  // TODO add just the workspace changed (O(1) time, currently O(n))
+      const savespace = data.val();
+      const keys = Object.keys(savespace);
+      for (let i = 0; i < keys.length; i++) {
+        const k = keys[i];
+        const name = savespace[k].name;
+        this.workspace_list.push({'id': i, 'name': name});
+      }
     }, (err) => {
       console.log(err);
     });
+
+    //   $(document).ready(function() {
+    //     $(document).on('click', '.SwalBtn1', function() {
+    //       // Some code 1
+    //       console.log('Button 1');
+    //       swal.clickConfirm();
+    //     });
+    //     $(document).on('click', '.SwalBtn2', function() {
+    //       // Some code 2
+    //       console.log('Button 2');
+    //       swal.clickConfirm();
+    //     });
+    //     $(document).on('click', '.SwalBtn3', function() {
+    //       // Some code 2
+    //       console.log('Button 3');
+    //       swal.clickConfirm();
+    //     });
+    //
+    //     $('#ShowBtn').click(function() {
+    //       swal({
+    //         title: 'Title',
+    //         html:
+    //           '<button type="button" role="button" tabindex="0" class="SwalBtn1 customSwalBtn">' + 'Button1' + '</button>' +
+    //           '<button type="button" role="button" tabindex="0" class="SwalBtn2 customSwalBtn">' + 'Button2' + '</button>' +
+    //           '<button type="button" role="button" tabindex="0" class="SwalBtn3 customSwalBtn">' + 'Button3' + '</button>',
+    //         showCancelButton: false,
+    //         showConfirmButton: false
+    //       });
+    //     });
+    //   });
+  }
+
+  button_callback_test(workspace_name: string) {
+    $(document).on('click', '.SwalBtn1', function () {
+      // Some code 1
+      console.log('Button 1');
+      swal.clickConfirm();
+    });
+    $(document).on('click', '.SwalBtn2', function () {
+      // Some code 2
+      console.log('Button 2');
+      swal.clickConfirm();
+    });
+    $(document).on('click', '.SwalBtn3', function () {
+      // Some code 2
+      console.log('Button 3');
+      swal.clickConfirm();
+    });
+
+    swal({
+      title: 'Title',
+      html:
+        '<button type="button" role="button" tabindex="0" class="SwalBtn1 customSwalBtn">' + 'Load' + '</button>' +
+        '<button type="button" role="button" tabindex="0" class="SwalBtn2 customSwalBtn">' + 'Delete' + '</button>' +
+        '<button type="button" role="button" tabindex="0" class="SwalBtn3 customSwalBtn">' + 'Set Up Notifications' + '</button>',
+      showCancelButton: false,
+      showConfirmButton: false
+    }).then();
   }
 
   button_callback(workspace_name: string) {
@@ -88,14 +147,17 @@ export class BlocklyComponent implements OnInit {
           'Load Complete',
           'Your workspace was successfully loaded.',
           'success'
-        ).then(() => {});
+        ).then(() => {
+        });
       } else if (result.dismiss === swal.DismissReason.cancel) { // Read more about handling dismissals
-        this.delete_workspace(workspace_name).then(() => {});
+        this.delete_workspace(workspace_name).then(() => {
+        });
         swalWithBootstrapButtons(
           'Deleted',
           'Your workspace was successfully deleted',
           'success'
-        ).then(() => {});
+        ).then(() => {
+        });
       }
     });
   }
@@ -125,7 +187,8 @@ export class BlocklyComponent implements OnInit {
         title: 'Your work has been saved',
         showConfirmButton: false,
         timer: 1500
-      }).then(() => {});
+      }).then(() => {
+      });
     } else {
       this.flashMessagesService.show(msg_fail, {timeout: 10000});
     }
@@ -134,7 +197,8 @@ export class BlocklyComponent implements OnInit {
   async delete_workspace(workspace_name: string) {
     const user_name = sessionStorage.getItem('user_name');
     const usersRef = this.firebaseService.database().ref(user_name);
-    usersRef.child(workspace_name).remove().then(() => {});
+    usersRef.child(workspace_name).remove().then(() => {
+    });
     BlocksService.clear();
     swal({
       position: 'center',
@@ -142,14 +206,16 @@ export class BlocklyComponent implements OnInit {
       title: 'Your work has been deleted',
       showConfirmButton: false,
       timer: 1500
-    }).then(() => {});
+    }).then(() => {
+    });
   }
 
   @ViewChild(ResultDisplayComponent)
-  set resultDisplayComponent (resultDisplay: ResultDisplayComponent) {
+  set resultDisplayComponent(resultDisplay: ResultDisplayComponent) {
     this.resultDisplay = resultDisplay;
     console.log('successfully captured child component: ', resultDisplay);
   }
+
   run_query(): void {
     this.twitterService.get_tweets(BlocksService.show_code()).subscribe({
       next: x => {
@@ -163,5 +229,20 @@ export class BlocklyComponent implements OnInit {
       error: err => console.log('cannot update, ', err),
       complete: () => console.log('query completed')
     });
+  }
+
+  swal3() {
+    // swal1({
+    //   type: 'warning',
+    //   title: 'numbers',
+    //   showCancelButton: true,
+    //   showAltActionButton: true,
+    //   confirmButtonColor: '#d33333',
+    //   cancelButtonColor: '#00417f',
+    //   altActionButtonColor: '#23c1c1',
+    //   altActionButtonText: 'One',
+    //   confirmButtonText: 'Two',
+    //   cancelButtonText: 'Three'
+    // })
   }
 }
